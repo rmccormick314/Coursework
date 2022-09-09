f783e74f0b7dcde1bea67094cee243620cea81b9
Richard <rmccormick314@gmail.com>
2022-09-08 20:34:00 -0700

    Update Homework2.py

--- CS499/Assignment 2/Homework2.py
+++ CS499/Assignment 2/Homework2.py
@@ -4,10 +4,17 @@ import urllib
 import urllib.request
 import pandas as pd
 import numpy as np
+import plotnine as p9
+
 import sklearn
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+from statistics import mode
 import warnings
 # <-- END IMPORTS / HEADERS -->
 
@@ -35,6 +42,7 @@ spam_label_col = 57
 zip_empty_col = 257
 kf = KFold(n_splits=3, shuffle=True, random_state=1)
 test_acc_df_list = []
+pipe = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1000))
 
 # <-- END INITIALIZATION -->
 
@@ -85,40 +93,72 @@ def main():
         'zip' : [zip_data, zip_labels]
     }
 
+    # Loop through each data set
     for data_set, (input_data, output_array) in data_dict.items():
+        # Output message for logging
         print("Working on set: " + str(data_set))
         current_set = str(data_set)
+        # Scale the data set
+        pipe.fit(input_data, output_array)
+
+        # Loop over each fold for each data set
         for foldnum, indicies in enumerate(kf.split(input_data)):
             print(foldnum)
+            # Set up input data structs
             index_dict = dict(zip(["train", "test"], indicies))
             param_dicts = [{'n_neighbors':[x]} for x in range(1, 21)]
+
+            # Establish different models
             clf = GridSearchCV(KNeighborsClassifier(), param_dicts)
+            linear_model = sklearn.linear_model.LogisticRegressionCV(cv=5)
+
+            # Creating dictionary with input and outputs
             set_data_dict = {}
             for set_name, index_vec in index_dict.items():
                 set_data_dict[set_name] = {
                     "X":input_data[index_vec],
                     "y":output_array.iloc[index_vec]
                 }
+
+            # Train the models with given data
             clf.fit(**set_data_dict["train"])
+            linear_model.fit(**set_data_dict["train"])
 
-            clf.best_params_
+            # Get most common output from outputs for featureless set
+            most_common_element = mode(output_array)
 
+            #print(clf.cv_results_)
+
+            # Get results
             cv_df = pd.DataFrame(clf.cv_results_)
             cv_df.loc[:, ["param_n_neighbors", "mean_test_score"]]
             pred_dict = {
-                "nearest_neighbors":clf.predict(set_data_dict["test"]["X"])
+                "nearest_neighbors":clf.predict(set_data_dict["test"]["X"]),
+                "linear_model":linear_model.predict(set_data_dict["test"]["X"]),
+                "featureless":most_common_element
             }
+
+            # Build results dataframe for each algo/fold 
             for algorithm, pred_vec in pred_dict.items():
-                test_ac_dict = {
+                test_acc_dict = {
                     "test_accuracy_percent":(
                         pred_vec == set_data_dict["test"]["y"]).mean()*100,
                     "data_set":data_set,
                     "fold_id":foldnum,
                     "algorithm":algorithm
                 }
-                test_acc_df_list.append(pd.DataFrame(test_ac_dict, index=[0]))
-    test_ac_df = pd.concat(test_acc_df_list)
-    print(test_ac_df)
+                test_acc_df_list.append(pd.DataFrame(test_acc_dict, index=[0]))
+
+    test_acc_df = pd.concat(test_acc_df_list)
+    print(test_acc_df)
+
+    plot = (p9.ggplot(test_acc_df,
+                        p9.aes(x='test_accuracy_percent',
+                        y='algorithm'))
+                   + p9.facet_grid('. ~ data_set')
+                   + p9.geom_point())
+
+    print(plot)
 
     print("\nCS 499: Homework 2 Program End")
     print("==============================\n")
